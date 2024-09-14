9372354b76d84746f7625fd139988b79d15a27ca
MoisesArr <96744545+MoisesArr@users.noreply.github.com>
2024-09-14 01:54:31 -0500

    reset all

--- dino-creator/src/data/dinosaurs.js
+++
@@ -1,8 +0,0 @@
-const dinosaurs = [
-    { id: 1, name: 'T-Rex', color: '#00FF00', feature: 'spikes' },
-    { id: 2, name: 'Triceratops', color: '#FF0000', feature: 'horns' },
-    // Add more dinosaurs as needed
-  ];
-  
-  export default dinosaurs;
-  
